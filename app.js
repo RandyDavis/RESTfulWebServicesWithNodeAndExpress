348a3a132a5ff46da10361237b37072adc4b49db
Randy Davis <randy_davis@outlook.com>
2016-01-03 20:53:39 -0600

    clean up code by separating bookRouter code into separate file

--- app.js
+++ app.js
@@ -1,62 +1,23 @@
-/**
- * Created by randy on 1/3/16.
- */
 var express = require('express'),
     mongoose = require('mongoose');
     bodyParser = require('body-parser');
+
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
 var Book = require('./models/bookModel');
 
-
 var app = express();
+
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-bookRouter.route('/Books')
-    .post(function(req, res) {
-        var book = new Book(req.body);
-        book.save();
-        res.status(201).send(book);
-    })
-
-    .get(function(req, res) {
-
-        var query = {};
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-        Book.find(query, function(err, books) {
-            if(err) {
-                res.status(500).send(err);
-            } else {
-                res.json(books);
-            }
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req, res) {
-
-
-        Book.findById(req.params.bookId,  function(err, book) {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.json(book);
-            }
-        });
-    });
 
 app.use('/api', bookRouter);
-
-
-
 
 
 app.get('/', function(req, res) {
